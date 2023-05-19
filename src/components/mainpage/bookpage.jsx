6b7012c0cf12ffb5544f399232103d35b9d40998
quelerir <lehandrk@yandex.ru>
2023-05-19 13:42:13 +0300

    rel 12 aleksey

--- src/components/mainpage/bookpage.jsx
+++ src/components/mainpage/bookpage.jsx
@@ -1,13 +1,14 @@
-import axios from "axios";
-import React, { useState } from "react";
-import CommentList from "../Comments/CommentList";
+import axios from 'axios';
+import React, { useState } from 'react';
+import CommentList from '../Comments/CommentList';
 
 export default function Bookpage({ book, comments, average }) {
   const [showComments, setShowComments] = useState(false);
   const [allComensts, setAllComments] = useState(comments);
 
+
   const [input, setInput] = useState({
-    commentbody: "",
+    commentbody: '',
   });
 
   const addHandler = (event) => {
@@ -20,7 +21,7 @@ export default function Bookpage({ book, comments, average }) {
     const response = await axios.post(`/api/addcommets/${book.id}`, input);
     console.log(response);
     setAllComments((prev) => [response.data, ...prev]);
-    setInput({ commentbody: "" });
+    setInput({ commentbody: '' });
   };
 
   const deleteHandler = async (commentId) => {
@@ -31,7 +32,7 @@ export default function Bookpage({ book, comments, average }) {
   };
   return (
     <div>
-      <div className="card mb-3" style={{ maxWidth: "1100px" }}>
+      <div className="card mb-3" style={{ maxWidth: '1100px' }}>
         <div className="row g-0">
           <div className="col-md-4">
             <img src={book.img} className="img-fluid rounded-start" alt="..." />
@@ -75,7 +76,7 @@ export default function Bookpage({ book, comments, average }) {
       <h5>Коментарии:</h5>
       {allComensts?.map((comment) => (
         <CommentList
-          key={comments.id}
+          key={comment.id}
           setAllComments={setAllComments}
           comment={comment}
           deleteHandler={deleteHandler}
